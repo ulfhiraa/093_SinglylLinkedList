1c3d4fe0d8ce60753c8c92b0511b376d7ed74446
ulfhiraa <studyiraa@gmail.com>
2023-05-19 15:15:29 +0700

    class List

--- SinglelLinkedList/SinglelLinkedList.cpp
+++ SinglelLinkedList/SinglelLinkedList.cpp
@@ -9,3 +9,15 @@ public:
 	char nama[20];
 	Node* next; 
 };
+
+class List
+{
+	Node* START;
+public:
+	List();
+	void addNote();
+	bool Search(int nim, Node** current, Node** previous);
+	bool listEmpty();
+	bool delNote(int element);
+	void tranverse();
+};
